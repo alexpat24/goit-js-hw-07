1dfb6c11d9584c238b6584510eef5b637b8020cb
alexpat24 <65975068+alexpat24@users.noreply.github.com>
2020-10-17 17:44:13 +0300

    Update task-02.js
    complete

--- task-02.js
+++ task-02.js
@@ -7,24 +7,41 @@ const ingredients = [
   "Приправы",
 ];
 
-const ingredientsRef = document.querySelector("#ingredients");
+const element = ingredients.map((num) => {
+  const newLi = document.createElement("li");
+  newLi.textContent = num;
+  return newLi;
+});
+const navEl = document.querySelector("#ingredients");
+navEl.append(...element);
 
-const potato = document.createElement("li");
-potato.textContent = ingredients[0];
+console.log(element);
 
-const mushrooms = document.createElement("li");
-mushrooms.textContent = ingredients[1];
+// const ingredients = ingredients.append(
+//   ingredients.map((el) => {
+//     const liRef = document.createElement("li");
+//     liRef.textContent = el;
+//     return liRef;
+//   })
+// );
+// const ingredientsRef = document.querySelector("#ingredients");
 
-const garlic = document.createElement("li");
-garlic.textContent = ingredients[2];
+// const potato = document.createElement("li");
+// potato.textContent = ingredients[0];
 
-const tomato = document.createElement("li");
-tomato.textContent = ingredients[3];
+// const mushrooms = document.createElement("li");
+// mushrooms.textContent = ingredients[1];
 
-const greens = document.createElement("li");
-greens.textContent = ingredients[4];
+// const garlic = document.createElement("li");
+// garlic.textContent = ingredients[2];
 
-const condiments = document.createElement("li");
-condiments.textContent = ingredients[5];
+// const tomato = document.createElement("li");
+// tomato.textContent = ingredients[3];
 
-ingredientsRef.append(potato, mushrooms, garlic, tomato, greens, condiments);
+// const greens = document.createElement("li");
+// greens.textContent = ingredients[4];
+
+// const condiments = document.createElement("li");
+// condiments.textContent = ingredients[5];
+
+//ingredientsRef.append(potato, mushrooms, garlic, tomato, greens, condiments);
